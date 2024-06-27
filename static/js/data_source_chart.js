7a1ae831052686b4b20e5a85f4038b6a901e4e68
Asif24extremum <157344114+Asif24extremum@users.noreply.github.com>
2024-06-27 18:56:50 +0530

    Update data_source_chart.js

--- static/js/data_source_chart.js
+++ static/js/data_source_chart.js
@@ -128,7 +128,81 @@
         }
     });
     
-    //Overall Readiness Chart
+//     //Overall Readiness Chart
+//     $(document).ready(function() {
+//         const socket = io();
+
+//         const ctx3 = $("#readiness_chart").get(0).getContext("2d");
+//         const myChart3 = new Chart(ctx3,{
+//             type: "pie",
+//             data: {
+//                 labels: ['Total Readiness', 'Data Left'],
+//                 datasets: [{
+//                     backgroundColor: [
+//                         "rgba(0, 156, 255, 0.7)",
+//                         "rgba(156, 0, 255, 0.7)",
+//                         "rgba(0, 0, 255, 0.7)",
+//                         "rgba(0, 0, 255, 0.1)",
+//                     ],
+//                     borderWidth: 1,
+//                     circumference: 180,
+//                     rotation : 270,
+//                     aspectRatio : 2,
+//                     borderRadius:8,
+//                     cutout: 95,
+//                     data: [75,25]
+//                 }]
+//             },
+//             options: {
+//                 responsive: true, 
+//                 plugins: {
+//                     legend: {
+//                         display: true
+//                     },
+//                     datalabels: {
+//                         color: '#ffffff',
+//                         // formatter: (value, ctx3) => {
+//                         //     let sum = 0;
+//                         //     let dataArr = ctx3.chart.data.datasets[0].data;
+//                         //     dataArr.map(data => {
+//                         //         sum += data;
+//                         //     });
+//                         //     let percentage = (value * 1000 / sum).toFixed(2) + "%";
+//                         //     return percentage;
+//                         // },
+//                         display: true,
+//                         align: 'center',
+//                         anchor: 'center'
+//                     }
+//                 }
+//             },
+//             plugins: [ChartDataLabels]
+//         });
+
+//         socket.on('update_gauge_chart', function(data) {
+//             // updateReadinessChart(data);
+//             if(data.pin==pin){
+//                 updateReadinessChart(data);
+//             }
+//             // else{
+//                 // document.getElementById('message').innerHTML = '<p>Login Pin Not Verified!</p>';
+//                 // setTimeout(function () {
+//                 //     document.getElementById('message').innerHTML = '';
+//                 // }, 8000);
+//             // }
+//         });
+    
+//         function updateReadinessChart(data) {
+//             myChart3.data.datasets[0].data = [
+//                 data.x,
+//                 100 - data.x
+//             ]
+//             myChart3.update(); // Refresh the chart
+//         }
+
+//     }); 
+// })(jQuery);
+
     $(document).ready(function() {
         const socket = io();
 
@@ -161,15 +235,6 @@
                     },
                     datalabels: {
                         color: '#ffffff',
-                        // formatter: (value, ctx3) => {
-                        //     let sum = 0;
-                        //     let dataArr = ctx3.chart.data.datasets[0].data;
-                        //     dataArr.map(data => {
-                        //         sum += data;
-                        //     });
-                        //     let percentage = (value * 1000 / sum).toFixed(2) + "%";
-                        //     return percentage;
-                        // },
                         display: true,
                         align: 'center',
                         anchor: 'center'
@@ -180,26 +245,19 @@
         });
 
         socket.on('update_gauge_chart', function(data) {
-            // updateReadinessChart(data);
-            if(data.pin==pin){
+            if(data.pin == pin){
                 updateReadinessChart(data);
             }
-            // else{
-                // document.getElementById('message').innerHTML = '<p>Login Pin Not Verified!</p>';
-                // setTimeout(function () {
-                //     document.getElementById('message').innerHTML = '';
-                // }, 8000);
-            // }
         });
-    
+
         function updateReadinessChart(data) {
+            let roundedValue = parseFloat(data.x).toFixed(2);
             myChart3.data.datasets[0].data = [
-                data.x,
-                100 - data.x
-            ]
+                roundedValue,
+                (100 - roundedValue).toFixed(2)
+            ];
             myChart3.update(); // Refresh the chart
         }
-
     }); 
-})(jQuery);
 
+})(jQuery);
