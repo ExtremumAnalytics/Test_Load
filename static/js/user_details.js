ed48c840428c24f312214fab01bf8b245daceea1
Akshat-Mittal-22 <akshat.mittal@extremumanalytics.com>
2024-07-05 01:51:21 +0530

    added db query in virtual analyst

---
+++ static/js/user_details.js
@@ -0,0 +1,8 @@
+// var socket = io();
+var pin = localStorage.getItem('pin');
+
+socket.on('userName', function(data) {
+    if(data.pin==pin){
+        document.getElementById('userName').innerHTML = `${data.userName}`;
+    } 
+});
