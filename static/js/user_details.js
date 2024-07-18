e9455756ec1da8f9e86d19cfe5b2f929600df4f8
Asif24extremum <157344114+Asif24extremum@users.noreply.github.com>
2024-07-18 18:16:26 +0530

    Delete static directory

--- static/js/user_details.js
+++
@@ -1,8 +0,0 @@
-// var socket = io();
-var pin = localStorage.getItem('pin');
-
-socket.on('userName', function(data) {
-    if(data.pin==pin){
-        document.getElementById('userName').innerHTML = `${data.userName}`;
-    } 
-});
