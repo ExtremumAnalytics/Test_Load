18819e2b088af9c201a6085ee840cfd09cf4e092
Akshat-Mittal-22 <akshat.mittal@extremumanalytics.com>
2024-07-25 17:11:57 +0530

    commit

--- static/js/user_details.js
+++
@@ -1,7 +0,0 @@
-var pin = localStorage.getItem('pin');
-
-socket.on('userName', function(data) {
-    if(data.pin==pin){
-        document.getElementById('userName').innerHTML = `${data.userName}`;
-    } 
-});
